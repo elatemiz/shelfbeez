9b9d906a600244c0194c98076f293a55d20d6cb7
elatemiz <elatemiz2726@gmail.com>
2026-08-28 22:42:02 +0300

    Display book count dynamically on profile

--- frontend/js/profile.js
+++ frontend/js/profile.js
@@ -1,8 +1,18 @@
 
+const profileName = document.querySelector("#profile-name");
 const profileUsername = document.querySelector("#profile-username");
 
 const storedUser = localStorage.getItem("user");
 const user = JSON.parse(storedUser);
 
-profileUsername.textContent = user.username;
+profileName.textContent = user.name;
+profileUsername.textContent = `@${user.username}`;
+
+const booksCount = document.querySelector("#books-count");
+
+const storedBooks = localStorage.getItem("books");
+const books = JSON.parse(storedBooks) || [];
+
+booksCount.textContent = books.length;
+
 

70afeaef4d7e1f6057fbb4eaaf33bfb4688c98a5
elatemiz <elatemiz2726@gmail.com>
2026-08-29 20:06:41 +0300

    Add book modal and form handling

--- frontend/js/profile.js
+++ frontend/js/profile.js
@@ -14,3 +14,49 @@ const books = user.books || [];
 booksCount.textContent = books.length;
 
 
+const addBookButton = document.querySelector("#open-add-book-btn");
+const addBookModal = document.querySelector("#add-book-modal");
+const cancelBookButton = document.querySelector("#cancel-book-btn");
+
+addBookButton.addEventListener("click", function(){
+     addBookModal.style.display = "flex";
+});
+
+cancelBookButton.addEventListener("click", function(){
+      addBookModal.style.display = "none";
+
+});
+
+const addBookForm = document.querySelector("#add-book-form");
+
+const bookTitleInput = document.querySelector("#book-title");
+const bookAuthorInput = document.querySelector("#book-author");
+const bookCoverInput = document.querySelector("#book-cover");
+const bookStatusInput = document.querySelector("#book-status");
+const bookRatingInput = document.querySelector("#book-rating");
+
+addBookForm.addEventListener("submit", function(event){
+     event.preventDefault();
+
+    //get input values:
+
+    const title = bookTitleInput.value.trim();
+    const author = bookAuthorInput.value.trim();
+    const cover = bookCoverInput.value.trim();
+    const status = bookStatusInput.value;
+    const rating = bookRatingInput.value;
+
+    //book object:
+
+    const book = {
+        id: Date.now(),
+        title: title,
+        author: author,
+        cover: cover,
+        status: status,
+        rating: Number(rating)
+    };
+
+    console.log(book);
+});
+
